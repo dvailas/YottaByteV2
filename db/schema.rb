6c42b52e0a3f897f8b9a83c4668cfd571e60dfb3
Ubuntu <vagrant@Base-Box>
2018-11-04 20:31:30 +0000

    Added Category Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_202937) do
+ActiveRecord::Schema.define(version: 2018_11_04_203059) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "security"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

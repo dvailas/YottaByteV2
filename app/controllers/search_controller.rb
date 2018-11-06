82b0fd6512fd7c840ee73f8aad5e094c1c7e2a4e
Ubuntu <vagrant@Base-Box>
2018-11-06 14:52:33 +0000

    Implemented search by category

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,16 @@ class SearchController < ApplicationController
   end
 
   def result
+    if params[:categories] != nil
+      @cat = params[:categories]
+      @product_collection = Product.where("name LIKE '%" + params[:keyword] + "%' OR
+                                          description LIKE '%" + params[:keyword] + "%' AND
+                                          category_id IN (" + @cat.join(",") + ")"  )
+
+    else
+      @product_collection = Product.where("name LIKE '%" + params[:keyword] + "%' OR
+        description LIKE '%" + params[:keyword] + "%'")
+    end
   end
 
   def recent

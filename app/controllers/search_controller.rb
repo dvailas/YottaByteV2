6e46f5435c15d40132dc9f035d486430e922e28d
Ubuntu <vagrant@Base-Box>
2018-11-06 02:18:03 +0000

    Added Recent view for filter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,6 @@ class SearchController < ApplicationController
   end
 
   def recent
-    @product_collection = Product.where("update_at >= ?", now()-7)
+    @product_collection = Product.where("updated_at >= ?", Date.today()-7)
   end
 end

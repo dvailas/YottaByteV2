68c5ac76a57d82bad285857d0855d792f40cc666
Ubuntu <vagrant@Base-Box>
2018-11-15 19:57:46 +0000

    Added Provinces Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_001743) do
+ActiveRecord::Schema.define(version: 2018_11_15_195700) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -64,6 +64,13 @@ ActiveRecord::Schema.define(version: 2018_11_05_001743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "tax"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "purchase_products", force: :cascade do |t|

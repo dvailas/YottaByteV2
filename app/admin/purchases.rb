a16330eef8f2793bc62cfc5cfe9150a1160bdcaf
Ubuntu <vagrant@Base-Box>
2018-11-15 19:23:34 +0000

    Added manipulation of Join Table in Purchase Table

--- app/admin/purchases.rb
+++ app/admin/purchases.rb
@@ -11,5 +11,48 @@ ActiveAdmin.register Purchase do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-permit_params :subtotal,:total,:user_id,:status
+permit_params :subtotal,:total,:user_id,:status, purchase_products_attributes:[:product_id, :purchase_id,:quantity,:price,:total, :_destroy]
+
+  index do
+    selectable_column
+      column :user
+      column :status
+      column :subtotal
+      column :total
+      column :created_at
+      actions
+  end
+
+  show do |purchase|
+    attributes_table do
+      row :user
+      row :status
+      row :subtotal
+      row :total
+      row :created_at
+      row :purchase_products do |purchase|
+        purchase.products.map {|p| p.name}.join(", ").html_safe
+      end
+    end
+  end
+
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs "Purchase" do
+      f.input :user
+      f.input :status
+      f.input :subtotal
+      f.input :total
+      f.has_many :purchase_products, allow_destroy: true do |n_f|
+        #n_f.object.purchase = :purchase
+        n_f.input :product
+        n_f.input :purchase
+        n_f.input :quantity
+        n_f.input :price
+        n_f.input :total
+      end
+    end
+    f.actions
+  end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,8 +1,10 @@
 class Purchase < ApplicationRecord
   belongs_to :user
-  has_many :purchase_products
+  has_many :purchase_products, :dependent => :destroy
   has_many :products, through: :purchase_products
 
   validates :user_id, :status, presence: true
   validates :subtotal, :total, numericality: true
+
+  accepts_nested_attributes_for :purchase_products, allow_destroy: true
 end
